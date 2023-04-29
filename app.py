6cee677dd5111d563c29adb6a03d7bd791ec2b4f
Akarsh Shrivastava <akarsh_shrivastava@yahoo.com>
2023-04-29 19:38:23 +0530

    Signup V1.0.1

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import boto3
 import json
+import logger
 
 def handler(event, context):
+    logger.info("event: " + str(event))
     print("event: " + str(event))
     print("context: " + str(context))
-    return json.dump(event)
+    logger.info("context: " + str(context))
+    return json.dumps(event)
     
