57670a280ac26e79ae2e53dc3ad01143ea288373
Akarsh Shrivastava <akarsh_shrivastava@yahoo.com>
2023-04-29 19:34:25 +0530

    Signup V1.0.1

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ import json
 def handler(event, context):
     print("event: " + str(event))
     print("context: " + str(context))
-    return event["queryStringParameters"]
+    return json.dump(event)
     
