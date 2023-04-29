fb13243dd49ab0f2ab7343752bb75f77bcd77da4
Akarsh Shrivastava <akarsh_shrivastava@yahoo.com>
2023-04-29 19:46:15 +0530

    Signup V1.0.1

--- app.py
+++ app.py
@@ -3,9 +3,7 @@ import json
 import logging
 
 def handler(event, context):
-    logging.info("event: " + str(event))
     print("event: " + str(event))
     print("context: " + str(context))
-    logging.info("context: " + str(context))
     return json.dumps(event)
     
