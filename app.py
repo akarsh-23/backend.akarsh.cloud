dda80c3b021f0ab18a0b3fe0bdcc63e0a2ecdb2f
Akarsh Shrivastava <akarsh_shrivastava@yahoo.com>
2023-04-29 20:22:10 +0530

    Signup V1.0.1

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
+from Model.User import User
 import boto3
 import json
 import logging
+import uuid
+
 
 def handler(event, context):
-    print("event: " + str(event))
-    print("context: " + str(context))
-    return event.get("resource",{"value":"Not Available"})
-    
+    user = User()
+    user.setId(uuid.uuid1())
+    user.setFirstname(event.get("firstname"))
+    user.setLastname(event.get("lastname"))
+    user.setEmail(event.get("email"))
+    user.setGender(event.get("gender"))
+    user.setPassword(event.get("password"))
+    response = "Something went wrong!!!"
+    try:
+        signedup = UserBO(user).signUp()
+        response = "You Have Been Signed Up!"
+    finally:
+        return response
