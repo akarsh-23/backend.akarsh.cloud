368844a7e485ef294bca08fcf3f5fb77ad904c4c
Akarsh Shrivastava <akarsh_shrivastava@yahoo.com>
2023-04-29 20:50:46 +0530

    Signup V1.0.1

--- app.py
+++ app.py
@@ -10,10 +10,20 @@ def handler(event, context):
     user.setLastname(event.get("lastname"))
     user.setEmail(event.get("email"))
     user.setGender(event.get("gender"))
-    user.setPassword(event.get("password"))
+    user.setPassword(event.get("pass"))
     response = "Something went wrong!!!"
     try:
         signedup = UserBO(user).signUp()
-        response = "You Have Been Signed Up!"
+        if signedup:
+            response = "You Have Been Signed Up!"
     finally:
         return response
+    
+if __name__ == "__main__":
+    handler({
+        "firstname":"Akarsh",
+        "lastname":"Shrivastava",
+        "email":"akarsh@akarsh.cloud",
+        "gender":"Male",
+        "pass":"akarsh@2301"
+    }, {})
